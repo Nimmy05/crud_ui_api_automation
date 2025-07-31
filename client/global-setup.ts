5ff48ea3b9216d5ab30dc8e2354703467abca47f
Nimmy Abraham Chandredath <nimmysmail@gmail.com>
2025-07-31 07:13:35 +0300

    refactor

--- client/global-setup.ts
+++ client/global-setup.ts
@@ -1,6 +1,6 @@
 import 'tsconfig-paths/register';
-import { constants, timeout } from 'globalConfig/constants';
-import { chromium, Locator, expect } from '@playwright/test';
+import { timeout } from 'globalConfig/constants';
+import { chromium, expect } from '@playwright/test';
 import { config } from 'dotenv';
 
 config();
@@ -19,48 +19,14 @@ async function globalSetup() {
   await page.fill('input[name="password"]', password);
   await page.click('button[type="submit"]');
 
-
   page.getByRole('button', { name: 'Login' }).click();
 
+  const alerts = page.getByRole('alert');
+  const loginAlert = alerts.filter({ hasText: `${email} is Logged In` }).first();
+  await expect(loginAlert).toBeVisible({ timeout });
 
-  // const alert: Locator = page.getByRole('alert').first(); // or better CSS/XPath/text locator
-  // await expect(alert).toBeVisible({timeout});
-
-  // Optionally capture alert text before it disappears
-//   const alerts = page.getByRole('alert');
-// const loginAlert = alerts.filter({ hasText: `${email} is Logged In` }).first();
-// await expect(loginAlert).toBeVisible({ timeout });
-// expect(loginAlert).toBeGreaterThan(2);
-
-
-  // Wait explicitly for alert/snackbar/toast with reduced timeout
-  // const alert = page.locator('role=alert').first(); // or better CSS/XPath/text locator
-  // await expect(alert).toBeVisible();
-
-  // // Optionally capture alert text before it disappears
-  // const alertText = await alert.innerText();
-  // console.log('Login Alert:', alertText);
-  // console.log('Login Alert:', `${email} is Logged In`);
-
-  // Continue with other assertions
-  // await expect(page.getByText(`${email} is Logged In`)).toBeVisible();
-  
   await page.waitForURL(`${baseUrl}/`)
-  // await expect(page.getByRole('heading', { level: 2 })).toHaveText(constants.headings.todo_list);
 
-  // try {
-  //   // Wait for input placeholder to ensure page is loaded after login
-  //   await page.waitForSelector(`input[placeholder='${constants.place_holder_texts.new_to_do}']`, { timeout: 80000 });
-
-  //   // Optionally wait for heading to confirm full load
-  //   await page.waitForSelector('text=Todo List', { timeout: 80000 });
-  // } catch (e) {
-  //   await page.screenshot({ path: 'login-failed.png' });
-  //   console.error('Login failed, page content:', await page.content());
-  //   throw e;
-  // }
-
-  // Save authenticated storage state to file for test reuse
   await page.context().storageState({ path: './storage/storageState.json' });
 
   await browser.close();
